c4f2a9d015444ba2ca9cb75ea42b11abecf62449
Claire Nollet <claire@artips.fr>
2020-11-13 15:46:42 +0100

    feat(calculator) begining js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,24 @@
 // JavaScript for the calculator
 
-// Si 0 est sélectionné,
+let number = 0
+
+// Si 0 est sélectionné, on stocke 0 dans number
 function zeroSelected() {
-    document.getElementById('screen').innerHTML = (0);
+    number = number + 0;
+    document.getElementById('screen').innerHTML = (number);
   }
 
+function oneSelected() {
+    number = number + 1;
+    document.getElementById('screen').innerHTML = (number);
+  }
+
+function cancelSelected() {
+    number = 0
+    document.getElementById('screen').innerHTML = (number);
+  }
+
+function equalSelected() {
+  let result = number
+  document.getElementById('screen').innerHTML = (result);
+}
