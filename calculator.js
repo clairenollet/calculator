fb0d1ffb82d146c85e4618c14b028f55985a647b
Claire Nollet <claire@artips.fr>
2020-11-20 18:00:39 +0100

    script

--- calculator.js
+++ calculator.js
@@ -1,24 +1,32 @@
 // JavaScript for the calculator
 
-let number = 0
+// Fonctionnement du script :
+// 1. on définit une variable data, qui est un string
+// 2. à chaque numéro sélectionné, on ajoute ce numéro à la chaîne de caractères data
+// 3. si la chaîne de caractères commence par 0 et que 0 n'est pas suivi d'une virgule, on doit enlever ce 0 (pas très grave car pas d'impact dans le calcul final)
+// 4. au clic sur cancel, data reprend sa valeur vide d'origine
+// 5. au clic sur égal, data est convertie en integer : la chaîne de caractère devient une opération dont le résultat est affiché dans l'élément screen
 
-// Si 0 est sélectionné, on stocke 0 dans number
-function zeroSelected() {
-    number = number + 0;
-    document.getElementById('screen').innerHTML = (number);
-  }
+// On définit la variable data, qui est un string
+let data = ""
 
-function oneSelected() {
-    number = number + 1;
-    document.getElementById('screen').innerHTML = (number);
-  }
+// On définit la variable number, qui est un string
+let number = selectedElement.to_s
 
+// Si Cancel est sélectionné, data reprend sa valeur d'origine
 function cancelSelected() {
-    number = 0
-    document.getElementById('screen').innerHTML = (number);
-  }
+  data = ""
+  document.getElementById('screen').innerHTML = (data);
+}
 
+// Si Equal est sélectionné, on transforme data en integer puis on affiche le résultat
 function equalSelected() {
-  let result = number
+  let result = data.to_i
   document.getElementById('screen').innerHTML = (result);
+}
+
+// Si un élément est sélectionné, on l'ajoute à la suite de data
+function zeroSelected() {
+    data += number
+    document.getElementById('screen').innerHTML = (data);
 }
