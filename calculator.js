ab917bb506d6bc26374947388be22f10cbcfc206
claire nollet <clairenlet@gmail.com>
2020-12-29 15:36:33 +0100

    feat(js): wip

--- calculator.js
+++ calculator.js
@@ -1,20 +1,6 @@
 // JavaScript for the calculator
 
-// Fonctionnement du script :
-
-// 1. Je déclare une variable sous forme d'array null : variable = [];
-
-// 2. Au clic sur "cancel", la variable retrouve sa valeur nulle d'origine ;
-
-// 3. Au clic sur un numéro : 
-
-// 4. Au clic sur un opérateur : 
-
-// 5. Au clic sur "résultat", je joins tous les items avec console.log(elements.join(''))
-
-/////////
-
-// 1. On définit la variable data
+// 1. On définit les variables générales
 let data = [];
 let res = null;
 let decimal = false;
@@ -25,7 +11,7 @@ function cancelSelected() {
   document.getElementById('screen').innerHTML = (data);
 };
 
-// 5. Si Equal est sélectionné, on affiche le résultat
+// 3. Si Equal est sélectionné, on affiche le résultat
 function equalSelected() {
   let nb1 = data[0]
   let nb2 = data[2]
@@ -49,58 +35,49 @@ function equalSelected() {
   // document.getElementById('screen').innerHTML = (eval(result));
 };
 
-// 3. Si un numéro est sélectionné, on regarde si le dernier element de data est un nombre ou un opérateur :
+// 4. Si un numéro ou la virgule est sélectionné, on regarde si le dernier element de data est un nombre ou un opérateur :
 function numberSelected(number) {
-// si c'est un nombre on l'ajoute à la suite
-
+// si on a sélectionné une virgule, on l'ajoute au dernier item
   if (number === '.') {
     decimal = true
     data[data.length -1] = data[data.length -1] + .0;
     tmp = data[data.length -1].toString() + '.';
-    // tmp = str(data[data.length -1])+'.';
   }
+  // si le denier item est un nombre, on y ajoute notre numéro
   else if (typeof data[data.length -1] == "number") {
+    // 1. si c'est un nombre décimal
     if (decimal) {
       // on transforme le dernier item de data en string
       strData = data[data.length -1].toString();
       // on localise la virgule
       decimalLocation = strData.indexOf('.');
+      // cas particulier
       if (decimalLocation === -1){
         decimaler = 10**-1
       }
-      // on récupère le nombre de chiffres après la virgule
+      // dans les autres cas, on récupère le nombre de chiffres après la virgule dans le dernier item de data
       else {
         i = strData.length - decimalLocation;
-        console.log(i); 
-        console.log(strData.length);
-        console.log(decimalLocation);
-        console.log(data[data.length -1]);
-      // ce résultat est le nb de 0 apres la virgule du multiplicateur
+      // ce résultat est le nombre de 0 qu'on met après la virgule du multiplicateur
         decimaler = 10**-i
       }
-  
-      // on prend le multiplicateur et on le * avec number
+      // on prend le multiplicateur et on multiplie number avec
       data[data.length -1] = data[data.length -1] + number*decimaler;
       tmp = data[data.length -1];
-
     }
+    // 2. si ce n'est pas un nombre décimal
     else {
       data[data.length -1] = data[data.length -1]*10 + number;
       tmp = data[data.length -1];
     }
   }
-// si c'est un opérateur on crée un nouvel item
+// si le denier item est un opérateur, on crée un nouvel item
   else {
      data.push(number);
      decimal = false;
      tmp = number;
     };
   document.getElementById('screen').innerHTML = (tmp);
-};
-
-// clic sur decimal
-function decimalSelected() {
-  numberSelected('.');
 };
 
 // 4. Au clic sur un opérateur : 
