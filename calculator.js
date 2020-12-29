5387a02112711cc7a3f08d34c430a3e808217e50
claire nollet <clairenlet@gmail.com>
2020-12-29 13:26:37 +0100

    feat(js): wip

--- calculator.js
+++ calculator.js
@@ -7,21 +7,10 @@
 // 2. Au clic sur "cancel", la variable retrouve sa valeur nulle d'origine ;
 
 // 3. Au clic sur un numéro : 
-// si ma variable est nulle (variable = []), le numéro est stocké en index 0 de la variable ;
-// si ma variable contient un nombre impair d'items (si variable.length % 2 != 0), le numéro est ajouté au dernier item ;
-// si ma variable contient un nombre pair d'items (si variable.length % 2 === 0), un nouvel item est créé pour stocker ce numéro ;
 
 // 4. Au clic sur un opérateur : 
-// si ma variable contient un nombre impair d'items, un nouvel item est créé dans l'array avec variable.push('opérateur sélectionné') ;
-// si ma variable contient un nombre pair d'items, je .pop() le dernier item, et j'ajoute l'opérateur sélectionné à sa place.
 
-// 5. Au clic sur "résultat", je joins tous les items avec console.log(elements.join('')); 
-// Je peux alors faire un return de l'opération (ou du nombre s'il n'y a pas eu d'opération) et l'afficher dans l'élément #screen de ma calculatrice.
-
-// TODO :
-// Gérer le 0 en tête d'item lorsqu'il n'y a pas de virgule après : il faudrait le supprimer, sinon ça pourrait donner des var = [010, -, 023]; par ex.
-// Écrire le script avec tous les if...else.
-// Établir le lien entre le script et le html, probablement avec un onClick par bouton de la calculatrice (mais il y a peut-être plus intelligent).
+// 5. Au clic sur "résultat", je joins tous les items avec console.log(elements.join(''))
 
 /////////
 
@@ -38,23 +27,20 @@ function cancelSelected() {
 function equalSelected() {
   let result = data.join('');
   document.getElementById('screen').innerHTML = (result);
+  // document.getElementById('screen').innerHTML = (eval(result));
 };
 
-// 3. Si un numéro est sélectionné :
+// 3. Si un numéro est sélectionné, on regarde si le dernier element de data est un nombre ou un opérateur :
 function numberSelected(number) {
-// si ma variable est nulle (variable = []), le numéro est stocké en index 0 de la variable ;
-  if (data.length === 0) {
-  data = [number];
+// si c'est un nombre on l'ajoute à la suite
+  if (typeof data[data.length -1] == "number") {
+    data[data.length -1] = data[data.length -1]*10 + number;
   }
-// si ma variable contient un nombre impair d'items, le numéro est ajouté au dernier item ;
-    else if (data.length % 2 != 0) {
-   // insertion du numéro à la fin du dernier item
-    }
-// si ma variable contient un nombre pair d'items, un nouvel item est créé pour stocker ce numéro ;
+// si c'est un opérateur on crée un nouvel item
     else {
-   data = data.push(number);
-    }
-    document.getElementById('screen').innerHTML = (data);
+     data.push(number);
+    };
+  document.getElementById('screen').innerHTML = (data);
 };
 
 // clic sur decimal
@@ -66,16 +52,15 @@ function decimalSelected() {
 function operatorSelected(operator) {
   // si ma variable est nulle, je fais une alerte
     if (data.length === 0) {
-      window.alert('Operation cannot start with an operator!');
+      window.alert('Operation cannot start with an operator');
     }
-  // si ma variable contient un nombre impair d'items, un nouvel item est créé dans l'array avec variable.push('opérateur sélectionné') ;
-      else if (data.length % 2 != 0) {
-        data = data.push(operator);
+       // si l'utilisateur sélectionne deux fois de suite un operateur on met une alerte
+      else if (typeof data[data.length -1] == 'string') {
+        window.alert('Operator cannot be selected after an operator');
       }
-  // si ma variable contient un nombre pair d'items, je .pop() le dernier item, et j'ajoute l'opérateur sélectionné à sa place.
+       // on crée un nouvel item
       else {
-        data = data.pop();
-        data = data.push(operator);
+        data.push(operator);
       };
       document.getElementById('screen').innerHTML = (data);
   };
